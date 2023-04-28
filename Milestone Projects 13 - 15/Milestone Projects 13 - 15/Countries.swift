fe13f5967d6e2df26437d1cfba19ccbba7c17148
Artem <artchzhen@gmail.com>
2023-04-28 19:27:48 +0900

    added file Countries and struct Countries

---
+++ Milestone Projects 13 - 15/Milestone Projects 13 - 15/Countries.swift
@@ -0,0 +1,12 @@
+//
+//  Countries.swift
+//  Milestone Projects 13 - 15
+//
+//  Created by Артем Чжен on 28/04/23.
+//
+
+import Foundation
+
+struct Countries: Codable {
+    var results: [Country]
+}
