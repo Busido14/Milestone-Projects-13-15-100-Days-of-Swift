27e274590985303072adf74ba726bc26022ebc6f
Artem <artchzhen@gmail.com>
2023-04-28 19:20:25 +0900

    added struct Country

--- Milestone Projects 13 - 15/Milestone Projects 13 - 15/Country.swift
+++ Milestone Projects 13 - 15/Milestone Projects 13 - 15/Country.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct Country {
+    var country: String
+    var capital: String
+    var languages: String
+    var area: Int
+    var population: Int
+    var currency:String
+}
